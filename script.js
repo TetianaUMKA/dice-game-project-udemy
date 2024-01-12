2ef2f1d5acb4b0cbbd040cbf69ae6ea03f89ef5d
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-01-12 21:14:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const totalScore0El = document.querySelector('#score--0');
-const totalScore1El = document.getElementById('score--1');
+const totalScore1El = document.querySelector('#score--1');
 const currentScore0El = document.querySelector('#current--0');
 const currentScore1El = document.querySelector('#current--1');
 
@@ -35,6 +35,13 @@ const currentTheme = function () {
   player1El.classList.toggle('player--active');
 };
 
+const switchPlayer = function () {
+  currentScore = 0;
+  showCurrent();
+  currentTheme();
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
+
 rollBtn.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
   diceEl.classList.remove('hidden');
@@ -44,28 +51,31 @@ rollBtn.addEventListener('click', function () {
     currentScore += dice;
     showCurrent();
   } else {
-    currentScore = 0;
-    showCurrent();
-    currentTheme();
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    switchPlayer();
   }
 });
 
 holdBtn.addEventListener('click', function () {
   totalScores[activePlayer] += currentScore;
   showTotal();
-  currentScore = 0;
-  showCurrent();
-  currentTheme();
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  if (totalScores[activePlayer] >= 100) {
+  if (totalScores[activePlayer] >= 20) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    switchPlayer();
   }
 });
 
 newGameBtn.addEventListener('click', function () {
   currentScore = 0;
-  totalScores = 0;
-  showCurrent();
-  showTotal();
-  currentTheme();
+  totalScores[0] = 0;
+  totalScores[1] = 0;
+  document.querySelector('#score--0').textContent = 0;
+  document.querySelector('#score--1').textContent = 0;
+  document.querySelector('.player--0').classList.add('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
 });
